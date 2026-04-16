e363e2607c15c242488cf572de2a756e32caed41
7anish <anissh946@gmail.com>
2026-04-16 15:08:09 +0530

    debug : url

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -11,3 +11,9 @@ export default defineConfig({
     url: process.env.DATABASE_URL,
   },
 });
+console.log("0");
+console.log(process.env.DATABASE_URL);
+console.log("1");
+console.log(process.env.DATABASE_URL!);
+console.log("2");
+console.log(process.env["DATABASE_URL"]);
